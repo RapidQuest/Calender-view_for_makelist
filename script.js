f1a4ccc5dc8dbaed04c5e50b021bcc16159086ce
star-step <67164566+star-step@users.noreply.github.com>
2021-08-25 23:49:44 +0530

    calendarmap and mounting of calender view done

--- script.js
+++ script.js
@@ -1,77 +1,94 @@
 import data from "./data.js"
 const app = new Vue({
-    el: "#calendar",
-    components: {
-      VueFullcalendar // make the <FullCalendar> tag available
+    el: "#app",
+    data() {
+        return {
+            list: data,
+        };
     },
-  
-    data: function() {
-      return {
-        calendarOptions: {
-          plugins: [
-            dayGridPlugin,
-            timeGridPlugin,
-            interactionPlugin // needed for dateClick
-          ],
-          headerToolbar: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay'
-          },
-          initialView: 'dayGridMonth',
-          initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
-          editable: true,
-          selectable: true,
-          selectMirror: true,
-          dayMaxEvents: true,
-          weekends: true,
-          select: this.handleDateSelect,
-          eventClick: this.handleEventClick,
-          eventsSet: this.handleEvents
-          /* you can update a remote database when these fire:
-          eventAdd:
-          eventChange:
-          eventRemove:
-          */
-        },
-        currentEvents: []
-      }
-    },
-  
-    methods: {
-  
-      handleWeekendsToggle() {
-        this.calendarOptions.weekends = !this.calendarOptions.weekends // update a property
-      },
-  
-      handleDateSelect(selectInfo) {
-        let title = prompt('Please enter a new title for your event')
-        let calendarApi = selectInfo.view.calendar
-  
-        calendarApi.unselect() // clear date selection
-  
-        if (title) {
-          calendarApi.addEvent({
-            id: createEventId(),
-            title,
-            start: selectInfo.startStr,
-            end: selectInfo.endStr,
-            allDay: selectInfo.allDay
-          })
-        }
-      },
-  
-      handleEventClick(clickInfo) {
-        if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-          clickInfo.event.remove()
-        }
-      },
-  
-      handleEvents(events) {
-        this.currentEvents = events
-      }
-    }
-  
-     
 
+    mounted() {
+      this.setUpCalenderView(),
+      this.calendarItems
+    },
+
+    methods: {
+      setUpCalenderView(){
+        console.log(this.calendarItems);
+        $(document).ready(function() {
+    
+          $('#calendar').fullCalendar({
+            header: {
+              left: 'prev,next today',
+              center: 'title',
+              right: 'month,basicWeek,basicDay'
+            },
+            // defaultDate: '2016-12-12',
+            navLinks: true, // can click day/week names to navigate views
+            editable: true,
+            eventLimit: true, // allow "more" link when too many events
+            events: this.calendarItems
+          });
+        });
+      },
+        getformatedDate(str) {
+            if (!str) {
+                console.log("undefined date string", str);
+                return "--";
+            }
+
+            const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", ];
+
+            const year = new Date(str).getFullYear().toString();
+            const monthNumber = new Date(str).getMonth().toString();
+            const day = new Date(str).getDate().toString();
+
+            if (day == undefined || monthNames[monthNumber] == undefined || year == undefined) return;
+            return `${monthNames[monthNumber]} ${day}`;
+        }
+    },
+
+    computed: {
+        calendarItems() {
+            if (!this.calendarViewMap) return;
+            const allEvents=[];
+            let event;
+              this.list.items.forEach(item => {
+                event = {
+                  title: item[this.calendarViewMap.map.title].toString(),
+                  start: item[this.calendarViewMap.map.startDate]
+                };
+                allEvents.push(event);
+              });
+              // console.log(typeof(allEvents));
+              return allEvents;
+        },
+
+        calendarViewMap() {
+            const fields = this.list.fields;
+            if (!fields) return;
+
+            const allDateFields = _.filter(fields, o => o.type === "date");
+
+            // if (!allChoiceFields.length) return;
+
+            const viewmap = {
+                    name: "calendar",
+                    map: {},
+                }
+            viewmap.map["title"] = fields[0]["name"];
+            // viewmap.map["id"] = "_id";
+
+              viewmap.map["startDate"] = (_.filter(fields, {
+                  type: "date",
+              })[0] || "")["name"];
+            if (allDateFields.length > 1) {
+            viewmap.map["endDate"] = (_.filter(fields, {
+                type: "date",
+            })[1] || "")["name"];
+          }
+            return viewmap;
+        },
+    }
 });
+console.log(app.calendarItems)
