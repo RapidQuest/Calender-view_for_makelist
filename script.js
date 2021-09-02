969a46958bd74c5970beeedbd59fb94de99e4027
star-step <67164566+star-step@users.noreply.github.com>
2021-09-02 16:28:54 +0530

    drag event to change date added and data update

--- script.js
+++ script.js
@@ -16,12 +16,34 @@ const app = new Vue({
       setUpCalenderView(){
         console.log(this.calendarItems);
         let allItems = this.calendarItems;
+        let allDataItems = this.list.items;
+        let mapItems =this.calendarViewMap;
     
         document.addEventListener('DOMContentLoaded', function() {
             var calendarEl = document.getElementById('calendar');
             var calendar = new FullCalendar.Calendar(calendarEl,{
             // defaultDate: '2016-12-12',
             initialView: 'dayGridMonth',
+            editable: true,
+            eventDrop: function(info){
+              allDataItems.forEach(item => {
+                if(info.event.id == item._id){
+                  item[mapItems.map.startDate] = info.event.start;
+                  console.log(item[mapItems.map.startDate]);
+                }
+              });
+            },
+              // alert(info.event.title + " was dropped on " + info.event.start.toISOString());
+
+              
+              // if (!confirm("Are you sure about this change?")) {
+              //   info.revert();
+              // }
+              // if(info.event.id == this.list.items._id){
+              //   this.list.items.calendarViewMap[startDate] =info.event.start;
+              //   console.log('done');
+              // }
+              // },
             // initialDate: '2021-07-07',
             headerToolbar: {
               left: 'prev,next today',
@@ -50,7 +72,7 @@ const app = new Vue({
 
             if (day == undefined || monthNames[monthNumber] == undefined || year == undefined) return;
             return `${monthNames[monthNumber]} ${day}`;
-        }
+        },
     },
 
     computed: {
@@ -98,3 +120,6 @@ const app = new Vue({
         },
     }
 });
+
+//use this when changing data
+// calendar.fullCalendar('refetchEvents');
